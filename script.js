235052dba51a9b5396bf353e23989baeb2a6cb73
Deja gordon <dejagordon80@gMAIL.COM>
2023-01-06 16:59:18 +0100

    refactored code to make more sense and added event listners

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ let playerTwo = {
     currentChoice: null
 };
 
-//create a player board with 2d array
+//change the color of the boxes on click
 
-const board = [
+let colorBox = document.querySelectorAll('.box');
+
+for(const b of colorBox){
+    b.addEventListener('click', boxClicked)
+}
+
+function boxClicked(e){
+    e.target.style.backgroundColor = 'green';
+}
+/* const board = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
@@ -23,19 +32,26 @@ let columns = board[0].length;
 //nested for loop to iterate through rows and columns
 for(let i = 0; i < rows.length; i++){
     for(let j = 0; j < columns.length; j++){
-        
+       
+     }
     }
-}
+
+  const div1 = document.querySelector('#div1');
+  div1.addEventListener('click', changeColor)
 
 //changeColor function
+
+function changeColor(){
+    document.div1.style.backgroundColor = 'yellow';
+}
+ */
+/* 
 const cells = document.querySelector('.parent').children;
 //change the color on the board for each individual cell
-cells.addEventListener('click', changeColor);
-function changeColor(){
-    this.style.backgroundColor = 'green';
-}
+cells.addEventListener('click', changeColor); */
 
-//when it's time to make the restart button it could be something like
+
+//when it's time to make the restart button functional it could be something like
 //if outcome is only tie/win then restart if button is clicked 
 
 
