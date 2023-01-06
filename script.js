6f4d763a5d43c72656d4ec25c448887027f127e5
Deja gordon <dejagordon80@gMAIL.COM>
2023-01-06 13:17:23 +0100

    updates

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //create variables for players
-
 let playerOne = {
 currentChoice: null
 };
@@ -10,7 +9,7 @@ let playerTwo = {
 
 //create a player board with 2d array
 
-let board = [
+const board = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
@@ -22,9 +21,19 @@ let rows = board.length;
 let columns = board[0].length;
 
 //nested for loop to iterate through rows and columns
+for(let i = 0; i < rows.length; i++){
+    for(let j = 0; j < columns.length; j++){
+        
+    }
+}
 
-//change the color on the board for each individual cell
+//changeColor function
 const cells = document.querySelector('.parent').children;
+//change the color on the board for each individual cell
+cells.addEventListener('click', changeColor);
+function changeColor(){
+    this.style.backgroundColor = 'green';
+}
 
 //when it's time to make the restart button it could be something like
 //if outcome is only tie/win then restart if button is clicked 
