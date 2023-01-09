3305ec61413bc4a07b356ae846b17b9e6c96e614
Deja gordon <dejagordon80@gMAIL.COM>
2023-01-09 15:57:12 +0100

    added portions of main function

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
 //create variables for players
-let playerOne = {
-currentChoice: null
-};
 
-let playerTwo = {
-    currentChoice: null
-};
+let currentPlayer = 'red';
+
+//player choice arrays
+
+playerOneChoices = [];
+
+playerTwoChoices = [];
 
 //change the color of the boxes on click
 
 let colorBox = document.querySelectorAll('.box');
 
-for(const b of colorBox){
-    b.addEventListener('click', boxClicked)
-}
+colorBox.forEach((box, index) => {
+    box.addEventListener('click', takeTurns)
+    console.log(index)
+})
 
-function boxClicked(e){
-    e.target.style.backgroundColor = 'green';
-}
-/* const board = [
+//possible winning combinations
+const winningCombinations = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
 ];
 
+//testing out a two player color
+function takeTurns(e){
+    // Change the box background color to match the currentPlayer
+    if (currentPlayer === 'red') {
+      e.target.style.backgroundColor = 'red';
+    } else {
+      e.target.style.backgroundColor = 'blue';
+    }
+// conditional for combinations 
+// first push each player choice to the choices array
+// then compare choices array with winning combinations
+    let divsArray = []; 
+   for (let i = 0, j = colorBox.length; i < j; i++){
+    divsArray.push(colorBox[i].getAttribute('id'))
+   }
+
+
+// Let's set up whos turns is it next based on the current value of currentPlayer
+    if (currentPlayer == 'red'){
+        currentPlayer = 'blue';
+    }else {
+        currentPlayer = 'red';
+    }
+
+    // Alert user on who's turn is it next
+
+}
+
+/* function blueFunction (){
+    this.style.backgroundColor = 'blue'
+}
+
+function redFunction (){
+    this.style.backgroundColor = 'red'
+}  */
+
+
+/*
 //declare columns and rows variables
 let rows = board.length;
 
