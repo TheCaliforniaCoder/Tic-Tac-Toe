f316d33f03724aaa7ac752a65d263b14a8471aa8
Deja gordon <dejagordon80@gMAIL.COM>
2023-01-10 14:23:14 +0100

    added functionality to start button

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 //create variables for current player and buttons
 
 let currentPlayer = 'red';
+let colorBox = document.querySelectorAll('.box');
 let restartButton = document.querySelector('#restart');
 let startButton = document.querySelector('#play-button');
+let alertMessage = document.querySelector('#alert');
 
-//change the color of the boxes on click
-
-let colorBox = document.querySelectorAll('.box');
-
-colorBox.forEach((box) => {
-    box.addEventListener('click', takeTurns)
-})
+//variable that will change once player wins or ties
+let gameOverPlayerWon = false;
 
 //possible winning combinations
 const winningCombinations = [
@@ -24,15 +21,24 @@ const winningCombinations = [
     [2, 5, 8]
 ];
 
-//player choice arrays
 
+
+startButton.addEventListener('click', startGame)
+
+function startGame () {
+    //change the color of the boxes on click
+colorBox.forEach((box) => {
+    box.addEventListener('click', takeTurns);
+})
+    startButton.addEventListener('click', restartGame);
+    currentPlayer = 'red';
+    alertMessage.textContent = `${currentPlayer}'s turn!`;
+}
+
+//player choice arrays
 let playerOneChoices = [];
 
 let playerTwoChoices = [];
-
-
-//variable that will change once player wins or ties
-let gameOverPlayerWon = false;
 
 //function that changes colors for each player and determines who's turn it is
 function takeTurns(e){
@@ -61,13 +67,15 @@ function takeTurns(e){
 //sets up who's turns is it next based on the current value of currentPlayer
     if (currentPlayer == 'red'){
         currentPlayer = 'blue';
-        console.log('blues turn')
+        // alert user on who's turn is it next
+        alertMessage.textContent = `${currentPlayer}'s turn!`;
     }else {
         currentPlayer = 'red';
-        console.log('reds turn')
+        // alert user on who's turn is it next
+        alertMessage.textContent = `${currentPlayer}'s turn!`;
     }
 
-// Alert user on who's turn is it next
+
     
 }
 
@@ -86,6 +94,15 @@ if (playerChoice.length >= 3){
 } else{
     return false;
 }
+}
+
+//restart game button
+restartButton.addEventListener('click',restartGame)
+
+function restartGame(){
+    currentPlayer = 'red';
+    alertMessage.textContent = `${currentPlayer}'s turn!`;
+    colorBox.forEach(box => box.style.backgroundColor = 'grey');
 
 }
 
@@ -95,44 +112,6 @@ if (playerChoice.length >= 3){
             }
     else if ()
 } */
-
-/* function blueFunction (){
-    this.style.backgroundColor = 'blue'
-}
-
-function redFunction (){
-    this.style.backgroundColor = 'red'
-}  */
-
-
-/*
-//declare columns and rows variables
-let rows = board.length;
-
-let columns = board[0].length;
-
-//nested for loop to iterate through rows and columns
-for(let i = 0; i < rows.length; i++){
-    for(let j = 0; j < columns.length; j++){
-       
-     }
-    }
-
-  const div1 = document.querySelector('#div1');
-  div1.addEventListener('click', changeColor)
-
-//changeColor function
-
-function changeColor(){
-    document.div1.style.backgroundColor = 'yellow';
-}
- */
-/* 
-const cells = document.querySelector('.parent').children;
-//change the color on the board for each individual cell
-cells.addEventListener('click', changeColor); */
-
-
 //when it's time to make the restart button functional it could be something like
 //if outcome is only tie/win then restart if button is clicked 
 
